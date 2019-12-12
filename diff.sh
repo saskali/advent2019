a01821fbfcb1efff4f05fc9d1af9df9af1a9db26
Saskia Lindner <saskia798@gmail.com>
2019-12-13 00:06:49 +0100

    Fix bash script for linux

--- diff.sh
+++ diff.sh
@@ -9,11 +9,11 @@ WIDTH=600
 
 SVG="${DEST}_diff.svg"
 
-git diff --word-diff=color --color-moved-ws=ignore-all-space --diff-algorithm=minimal --no-index "$PART1" "$PART2" \
+git diff --word-diff=color --diff-algorithm=minimal --no-index "$PART1" "$PART2" \
   | ansifilter -S --width $WIDTH --height __HEIGHT__ \
   > "${SVG}"
 
-HEIGHT=$(grep -o 'y="\d*"' "${SVG}" | tail -1 | grep -o '\d\+')
+HEIGHT=$(grep -o 'y=\"[0-9]*\"' "${SVG}" | tail -1 | grep -o '[0-9]\+')
 HEIGHT=$((HEIGHT + 20))
 
 sed "s/__HEIGHT__/${HEIGHT}/g" "${SVG}" > "${SVG}.tmp"
